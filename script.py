e9e6aad5b92b9377d84ac550afc44055e38925aa
APacheco37 <apacheco73@hotmail.com>
2020-10-21 23:11:33 -0300

    Add comments to script file

--- script.py
+++ script.py
@@ -1,10 +1,12 @@
 import os
 import random
 
+# Clears the terminal screen, and displays a title bar.
 def displayTitleBar():
     os.system("clear")
     print("------- Match engine simulator -------\n")
 
+# Simulates a match between two teams and displays the result
 def playMatch(firstTeam, secondTeam):
     a = int(input("\n" + firstTeam + " level: "))
     b = int(input(secondTeam + " level: "))
@@ -15,13 +17,15 @@ def playMatch(firstTeam, secondTeam):
     else:
         print("\n" + firstTeam + " won")
     
-
+# Displays the available list of commands/actions
 def printCommands():
     print("Available commands:")
     print("[1] Play match")
     print("[2] Change team names")
     print("[q] Quit program\n")
 
+
+# MAIN PROGRAM
 command = ""
 firstTeam = "First team"
 secondTeam = "Second team"
